878ea5c1064aa17c9fa709d504b1a84063fc9a70
Harshu1820 <126434520+Harshu1820@users.noreply.github.com>
2023-05-12 19:14:19 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,37 @@
+const Object1 = {
+    name: "Manish",
+    count: [1, 2, 3, 4, 5]
+};
+
+function changeName() {
+    Object1.name = "Manish Fale";
+    const multiply = (x, y) => x * y;
+    return multiply;
+}
+
+const result = changeName()(2, 3);
+
+const object2 = {
+    ...Object1,
+  
+    date: function getCurrentDateTime() {
+      const currentDate = new Date();
+      const year = currentDate.getFullYear();
+      const month = ('0' + (currentDate.getMonth() + 1)).slice(-2);
+      const day = ('0' + currentDate.getDate()).slice(-2);
+  
+      return `${year}-${month}-${day}`;
+    },
+    
+};
+
+console.log(Object1);
+
+changeName();
+
+Object1.date = object2.date();
+
+console.log(Object1);
+console.log(result);
+console.log(object2);
+console.log(object2.date());
